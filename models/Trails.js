5ea5c14cc2eb2a687bf5701d3214b52a9806cae0
Brenner Jolley <bjbrenardjoey@gmail.com>
2024-04-03 21:01:16 -0600

    added trails model

---
+++ models/Trails.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const sequelize = require('./Review');
+
+class Trails extends Model { }
+
+Trails.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        trail_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        location: {
+            type: DataTypes.STRING,
+        },
+        conditions: {
+            type: DataTypes.STRING,
+        },
+        review: {
+            type: DataTypes.STRING,
+        }
+    },
+);
