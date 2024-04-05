160819bebdfad9c50b51267277f95463fe925829
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-04-04 20:44:00 -0600

    added homepage-route, index.js route, and user.js route

---
+++ controllers/api/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes');
+const contactRoutes = require('./contact-routes');
+const aboutRoutes = require('./about-routes');
+const loginRoutes = require('./login-routes');
+const trailRoutes = require('./trails-routes')
+
+router.use('/users', userRoutes);
+router.use('/contact', contactRoutes);
+router.use('/about', aboutRoutes);
+router.use('/login', loginRoutes);
+router.use('/trails', trailRoutes);
+
+module.exports = router;
