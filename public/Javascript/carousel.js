5d4e4c56147d8ad5623ce759b0f4225e1a8d7b4d
PCast71 <patrickc77hhs@gmail.com>
2024-04-09 08:03:15 -0600

    added cards and working on carousel for explore page, it isnt functioning properly, need to troubleshoot.

---
+++ public/Javascript/carousel.js
@@ -0,0 +1,14 @@
+function showSlides () {
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+
+  slides[slideIndex].style.display = 'block';
+  setTimeout(showSlides, 4000);
+}
